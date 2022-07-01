e2a74070dafbb945479bf25c754555d2e206c4c9
Kofi Oghenetega <kofitega@gmail.com>
2022-07-01 16:58:20 -0700

    betty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,13 +15,13 @@ int main(void)
 			printf("Last digit of %d is ", n);
 			if ((n % 10) > 5)
 			{
-				printf(" and is greater than 5");
+				printf("%d  and is greater than 5");
 			} else if ((n % 10) == 0)
 			{
-				printf(" and is 0");
+				printf("%d  and is 0");
 			} else if ((n % 10) < 6 && (n % 10 != 0))
 			{
-				printf(" and is less than 6");
+				printf("%d and is less than 6");
 			}
 			return (0);
 
