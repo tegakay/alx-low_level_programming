31dc96e28494fec1efd3dac6b65f2e4127c974d6
Kofi Oghenetega <kofitega@gmail.com>
2022-07-02 02:54:46 -0700

    ez

---
+++ 0x01-variables_if_else_while/3-prnt_alphabets.c
@@ -0,0 +1,16 @@
+#Include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
+
