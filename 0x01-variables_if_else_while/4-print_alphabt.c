10441f404d972aa0e1d5bab315e9be263285a210
Kofi Oghenetega <kofitega@gmail.com>
2022-07-03 08:36:36 -0700

    2

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,10 +11,11 @@ int main(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		if (letter != 'e' || letter != 'q')
+		if (letter == 'e' || letter == 'q')
 		{
-			putchar(letter);
+			continue;
 		}
+		putchar(letter);
 	}
 	putchar('\n');
 	return (0);
