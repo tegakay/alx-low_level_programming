e6d8a5738a017f1c8b2ec184435660cb51b91b97
Kofi Oghenetega <kofitega@gmail.com>
2022-07-03 09:35:39 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar(num);
+	putchar('n');
+	return (0);
+}
