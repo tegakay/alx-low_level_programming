88e79a8ba2dd188a77e2697a756143a441794a25
Kofi Oghenetega <kofitega@gmail.com>
2022-07-06 14:53:05 -0700

    data

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+/*
+ * File: 1-alphabet.c
+ * Auth: Dr Marcus imagwe
+ */
+
+#include "main.h"
+
+/**
+ *
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ *
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 97; a <= 122; a++)
+	_putchar(alpha);
+	_putchar('\n');
+
+}
