0f5c6991af22029d23a690b8113767efcdecb448
Kofi Oghenetega <kofitega@gmail.com>
2022-07-12 07:28:58 -0700

    no2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"                                                                                                                                              
+
+/**                                                                              *swap_int - takes a pointer to an int as parameter                              * @a: parameter take the value.  
+ * @b: parameter take the value.                                                 */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+
+	*a = *b;
+	*b = c;
+}
+
+                                                                                                                                
+
+
+		                                                                                                                                
+
