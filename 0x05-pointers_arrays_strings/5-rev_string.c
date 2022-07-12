b5d633c6b9d16d652ceee90966aa1634fcc3c48a
Kofi Oghenetega <kofitega@gmail.com>
2022-07-12 11:33:22 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,20 +3,21 @@
 /**
  * rev_string - print a string in reverse
  * returns - string in reverse
+ * @s: input param
  */
 
 void rev_string(char *s)
 {
 	int i = 0, len = 0;
 
-	char tmp;
+	char tmpe;
 
 	while (s[i++])
 		len++;
 	for (i = len - 1; i >= len / 2; i--)
 	{
-		tmp = s[i];
+		tmpe = s[i];
 		s[i] = s[len - i - 1];
-		s[len - i - 1] = tmp;
+		s[len - i - 1] = tmpe;
 	}
 }
