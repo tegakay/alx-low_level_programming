5ffd2d95ef50e7eab2d89872adc11d15e0f97320
Kofi Oghenetega <kofitega@gmail.com>
2022-07-28 14:07:15 -0700

    more malloc

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+int *array_range(int min, int max);
+
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+void *malloc_checked(unsigned int b);
+
+#endif/*MAIN_H*/
