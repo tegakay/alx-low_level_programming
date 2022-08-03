2401f98258c8ddf52b06c67881cfaf915e23baf0
Kofi Oghenetega <kofitega@gmail.com>
2022-08-03 12:03:54 -0700

    new task

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - a function to print character..
+ * @array: array to print
+ * @size: is the size of the array
+ * @action:  is a pointer to the function you need to use
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array && action)
+	{
+		i = 0;
+		while (i < size)
+			{
+				action(array[i]);
+				i++;
+			}
+	}
+}
