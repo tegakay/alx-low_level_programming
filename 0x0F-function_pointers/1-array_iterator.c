af9d04fffd4f8f810a6c9b5ee4f49023ced7174e
Kofi Oghenetega <kofitega@gmail.com>
2022-08-03 12:14:20 -0700

    fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,11 +14,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array && action)
 	{
-		i = 0;
-		while (i < size)
-			{
-				action(array[i]);
-				i++;
-			}
+	i = 0;
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
 	}
 }
