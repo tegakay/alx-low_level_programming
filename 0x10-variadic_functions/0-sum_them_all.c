8b2efa1febd1366de58b24a9ae94833e32aeaae2
Kofi Oghenetega <kofitega@gmail.com>
2022-08-04 09:19:54 -0700

    data

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,6 +14,8 @@ int sum_them_all(const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
+	if(n == 0)
+		return (0);
 	for (ar = 0; ar < n; ar++)
 		sum += va_arg(args, int);
 	va_end(args);
