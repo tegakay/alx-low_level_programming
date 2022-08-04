9d638f9b9f39c252e9aa02c12d1986b2d5b5ccbd
Kofi Oghenetega <kofitega@gmail.com>
2022-08-04 09:38:20 -0700

    new

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,11 +10,10 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int ar;
 	int sum = 0;
-
 	va_list args;
 
 	va_start(args, n);
-	if(n == 0)
+	if (n == 0)
 		return (0);
 	for (ar = 0; ar < n; ar++)
 		sum += va_arg(args, int);
