08daac7ef8f6c37c2ff1e87f829f83596914ba69
Kofi Oghenetega <kofitega@gmail.com>
2022-08-04 09:16:42 -0700

    due

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function to sum
+ * @n: numbers to sum
+ * Return:sum of numbes
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int ar;
+	int sum = 0;
+
+	va_list args;
+
+	va_start(args, n);
+	for (ar = 0; ar < n; ar++)
+		sum += va_arg(args, int);
+	va_end(args);
+	return (sum);
+}
