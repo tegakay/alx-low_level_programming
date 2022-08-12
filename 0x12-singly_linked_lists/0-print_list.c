6303cd96792d4ef6af8da8c662bd003365244132
Kofi Oghenetega <kofitega@gmail.com>
2022-08-12 13:29:05 -0700

    due

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the list link.
+ * @h: pointer to the list_t list to print
+ * Return: the number of nodes printed
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		node++;
+		h = h->next;
+	}
+	return (node);
+}
