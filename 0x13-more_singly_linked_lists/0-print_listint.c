8480cfe0d94346116a5ff69d998a5d3ac0813692
Kofi Oghenetega <kofitega@gmail.com>
2022-08-16 07:52:28 -0700

    new progress

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - print elements of a list
+ * @h: -the list
+ * Return:the no of elements
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t plus = 0;
+
+	while (h)
+	{
+		printf("%d \n",h->n);
+		plus++;
+		h = h->next;
+	}
+	return (plus);
+}
